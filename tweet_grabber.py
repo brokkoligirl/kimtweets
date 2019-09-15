c48a31216e10293b3020de046999eb4b44c6421b
Karolin Flach <karolinflach@gmail.com>
2019-09-16 01:13:11 +0200

    add more charts and explanations

--- tweet_grabber.py
+++ tweet_grabber.py
@@ -1,39 +1,49 @@
-import GetOldTweets3 as got
+import GetOldTweets3 as Got3
 import csv
 import datetime
 
 
-def tweet_grabber(usr_name, n, filename):
+def grab_all_tweets(usr_name, n, filename, mode='w', date_since=None):
 
     """
-    returns a csv file with the last n tweets of a twitter user
+    returns a csv file with the n most recent tweets of a twitter user
 
-    :param usr_name: twitter user name whose tweets we want to download
-    :param n: maximum number of tweets to download. will grab all tweets if set to 0
+    :param usr_name: twitter user name whose tweets are to be downloaded
+    :param n: maximum number of tweets to download. will grab all avilable tweets if set to 0
     :param filename: name of the csv file to which the tweets will be saved
+    :param date_since: (str. "yyyy-mm-dd"): lower bound date (UTC) to restrict search. default is None.
+    :param mode: a or w
 
     depending on the amount of tweets in the account, this can take 20+ minutes
     """
 
-    tweet_criteria = got.manager.TweetCriteria().setUsername(usr_name) \
-                                                .setMaxTweets(n)
+    if date_since is None:
 
-    tweets = got.manager.TweetManager.getTweets(tweet_criteria)
+        tweet_criteria = Got3.manager.TweetCriteria().setUsername(usr_name) \
+                                                     .setMaxTweets(n)
+
+    else:
+
+        tweet_criteria = Got3.manager.TweetCriteria().setUsername(usr_name) \
+                                                     .setMaxTweets(n) \
+                                                     .setSince(date_since)
+
+    tweets = Got3.manager.TweetManager.getTweets(tweet_criteria)
 
     column_names = ["username", "date", "retweets", "favorites", "text", "geo",
-                    "mentions", "to", "hashtags", "id", "permalink"]
+                    "mentions", "hashtags", "id", "permalink"]
 
     counter = 0
 
-    with open(filename, 'w', newline='') as csvfile:
+    with open(filename, mode, newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(column_names)
+        if mode == 'w':
+            writer.writerow(column_names)
 
         for tweet in tweets:
             tweet_id = tweet.id
             permalink = tweet.permalink
             username = tweet.username
-            to = tweet.to
             text = tweet.text
             date = tweet.date
             retweets = tweet.retweets
@@ -43,16 +53,18 @@ def tweet_grabber(usr_name, n, filename):
             geo = tweet.geo
 
             tweetrow = [username, date, retweets, favorites, text, geo,
-                        mentions, to, hashtags, tweet_id, permalink]
+                        mentions, hashtags, tweet_id, permalink]
 
             writer.writerow(tweetrow)
-            counter +=1
+            counter += 1
 
     print('finished saving tweets. total number of tweets downloaded: ', counter)
 
 
-beginning = datetime.datetime.now()
-print(beginning)
-tweet_grabber('kimkardashian', 0, 'kim.csv')
-ending = datetime.datetime.now()
-print(ending)
+if __name__ == '__main__':
+
+    beginning = datetime.datetime.now()
+    print(beginning)
+    grab_all_tweets('kimkardashian', 0, 'allkim_copy_for_use.csv', "a", "2019-07-22")
+    ending = datetime.datetime.now()
+    print(ending)
